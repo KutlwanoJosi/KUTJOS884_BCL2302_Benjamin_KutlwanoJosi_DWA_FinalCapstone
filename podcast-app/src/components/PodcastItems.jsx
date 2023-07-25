7aafed180d110dfafa3d287f9ffee93766f77bdd
Kutlwano Josi <kutlwanojosi01@gmail.com>
2023-07-25 20:46:51 +0200

    Added my card as a PodcastItem function

---
+++ podcast-app/src/components/PodcastItems.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const PodcastItem = ({ podcast }) => {
+  return (
+    <div>
+      <h3>{podcast.title}</h3>
+      <p>{podcast.description}</p>
+      <audio controls>
+        <source src={podcast.audio} type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
+    </div>
+  );
+};
+
+export default PodcastItem;
