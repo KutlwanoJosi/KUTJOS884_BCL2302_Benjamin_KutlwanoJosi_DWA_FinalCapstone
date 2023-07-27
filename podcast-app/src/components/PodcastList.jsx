d6ef409844dac58175cb594119cae36a86230206
Kutlwano Josi <kutlwanojosi01@gmail.com>
2023-07-27 10:01:55 +0200

    installed bootstrap and imported its CSS

--- podcast-app/src/components/PodcastList.jsx
+++ podcast-app/src/components/PodcastList.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import PodcastItem from './PodcastItem';
+import React from "react";
+import "./PodcastItem.css";
 
-const PodcastList = ({ podcasts }) => {
+const PodcastItem = ({ podcast }) => {
   return (
-    <div className="podcast-list">
-      {podcasts.map((podcast) => (
-        <PodcastItem key={podcast.id} podcast={podcast} />
-      ))}
+    <div className="podcast-item">
+      <img src={podcast.image} alt={`Podcast - ${podcast.title}`} />
+      <h3>{podcast.title}</h3>
+      <p>{podcast.description}</p>
+      <audio controls>
+        <source src={podcast.audio} type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio>
     </div>
   );
 };
 
-export default PodcastList;
+export default PodcastItem;
