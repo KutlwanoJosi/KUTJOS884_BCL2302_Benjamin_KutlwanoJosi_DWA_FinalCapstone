3eaf18dfde7cdd811c970fb319faf87561e9040f
Kutlwano Josi <kutlwanojosi01@gmail.com>
2023-08-03 22:35:24 +0200

    Updated my PodcastItems.jsx

--- podcast-app/src/components/PodcastItems.jsx
+++ podcast-app/src/components/PodcastItems.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { SlButton, SlCard, SlRating } from '@shoelace-style/shoelace/dist/react';
 import "./PodcastItem.css"; // Import the custom CSS file
 
 const PodcastItem = ({ podcast }) => {
@@ -9,42 +8,53 @@ const PodcastItem = ({ podcast }) => {
     setShowOverlay((prevShowOverlay) => !prevShowOverlay);
   };
 
-  return (
-    <div className={`podcast-item ${showOverlay ? "show-overlay" : ""}`}>
-      <SlCard className="card-overview" onClick={handleToggleOverlay}>
-        <img
-          slot="image"
-          src={podcast.image}
-          alt={`Podcast - ${podcast.title}`}
-          height="200"
-          width="200"
-        />
+  const genreMapping = {
+    1: "Personal Growth",
+    2: "True Crime and Investigative Journalism",
+    3: "History",
+    4: "Comedy",
+    5: "Entertainment",
+    6: "Business",
+    7: "Fiction",
+    8: "News",
+    9: "Kids and Family",
+  };
 
-        <strong>{podcast.title}</strong>
-        <br />
-        <div slot="footer">
-          <SlButton variant="primary" pill>
-            More Info
-          </SlButton>
-          <SlRating></SlRating>
-        </div>
-      </SlCard>
+  const genreTitles = podcast.genres.map((genreId) => genreMapping[genreId]);
+
+  return (
+    <div
+      className={`podcast-item ${showOverlay ? "show-overlay" : ""}`}
+      onClick={handleToggleOverlay}
+    >
+      {/* Display the podcast image with fixed height and width */}
+      <h3>{podcast.title}</h3>
+      <img
+        src={podcast.image}
+        alt={`Podcast - ${podcast.title}`}
+        height="200"
+        width="200"
+      />
 
       <div className="overlay">
-        <h3>{podcast.title}</h3>
-        {showOverlay ? (
-          <p className="description">{podcast.description}</p>
-        ) : (
-          <p className="description-preview">
-            {podcast.description.slice(0, 100)}...
-          </p>
-        )}
+        <div
+          className="podcast-item"
+          onClick={() => handlePodcastClick(podcast)}
+        >
+          <h2>{podcast.title}</h2>
+          <p>{podcast.description}</p>
+          <strong>
+            <p>Genre: {genreTitles.join(", ")}</p>
+          </strong>
+        </div>
       </div>
 
-      <audio controls>
-        <source src={podcast.audio} type="audio/mpeg" />
-        Your browser does not support the audio element.
-      </audio>
+      <div className="seasons">
+        <strong>
+          <p>Seasons: {podcast.seasons}</p>
+        </strong>
+      </div>
+
     </div>
   );
 };
