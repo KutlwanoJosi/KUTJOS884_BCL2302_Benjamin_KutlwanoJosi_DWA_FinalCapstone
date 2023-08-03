b6c82519be7f773a14c8282de57d4dcae99da521
Kutlwano Josi <kutlwanojosi01@gmail.com>
2023-08-03 22:36:20 +0200

    Added Sorting and Mapping to my PodcastList.jsx

--- podcast-app/src/components/PodcastList.jsx
+++ podcast-app/src/components/PodcastList.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
-import PodcastItem from './PodcastItems';
-import './PodcastList.css'; 
+import React, { useState, useEffect } from 'react';
+import PodcastItem from './PodcastItem';
+import './PodcastList.css';
 
-const PodcastList = ({ podcasts }) => {
+const PodcastList = ({ podcasts, sortDirection, selectedGenre }) => {
+  const [sortedPodcasts, setSortedPodcasts] = useState([]);
+
+  useEffect(() => {
+    // Apply sorting
+    const sorted = [...podcasts];
+    sorted.sort((a, b) => {
+      const titleA = a.title.toLowerCase();
+      const titleB = b.title.toLowerCase();
+      if (sortDirection === 'asc') {
+        return titleA.localeCompare(titleB);
+      } else {
+        return titleB.localeCompare(titleA);
+      }
+    });
+
+    // Apply filtering based on selectedGenre
+    const filteredPodcasts = selectedGenre
+      ? sorted.filter((podcast) => podcast.genres.includes(parseInt(selectedGenre)))
+      : sorted;
+
+    setSortedPodcasts(filteredPodcasts);
+  }, [podcasts, sortDirection, selectedGenre]);
+
   return (
     <div className="podcast-list">
-      {podcasts.map((podcast) => (
+      {sortedPodcasts.map((podcast) => (
         <PodcastItem key={podcast.id} podcast={podcast} />
       ))}
     </div>
